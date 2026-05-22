39cf7da0b3d72d410066935de5ef1b8add072ab7
Mehedi Hasan <mehedihasanhridoy20@gmail.com>
2026-05-22 20:31:33 +0300

    add swagger ui

--- src/main/java/com/mehedi/task_api/RESTCONTROLLER/ProjectController.java
+++ src/main/java/com/mehedi/task_api/RESTCONTROLLER/ProjectController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.mehedi.task_api.service.ProjectService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/projects")
+@Tag(name = "Project API", description = "Endpoints for managing projects")
 public class ProjectController {
     private final ProjectService projectService;
 
@@ -26,12 +30,14 @@ public class ProjectController {
         this.projectService = projectService;
     }
 
+    @Operation(summary = "Get all projects", description = "Retrieve a list of all projects")
     @GetMapping
     public List<ProjectDTO> getAllProjects() {
         List<Project> projects = projectService.getAllProjects();
         return projects.stream().map(project -> ProjectMapper.toProjectDTO(project)).collect(Collectors.toList());
     }
 
+    @Operation(summary = "Create a new project", description = "Create a new project with the provided details")
     @PostMapping
     public ProjectDTO createProject(@RequestBody Project project) {
         Project savedProject = projectService.createProject(project);
--- src/main/java/com/mehedi/task_api/RESTCONTROLLER/TaskController.java
+++ src/main/java/com/mehedi/task_api/RESTCONTROLLER/TaskController.java
@@ -10,6 +10,10 @@ import com.mehedi.task_api.DTO.TaskDTO;
 import com.mehedi.task_api.mapper.TaskMapper;
 import com.mehedi.task_api.model.Task;
 import com.mehedi.task_api.service.TaskService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,6 +26,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/api/tasks")
+@Tag(name = "Task API", description = "Endpoints for managing tasks")
 public class TaskController {
 
     private final TaskService taskService;
@@ -30,12 +35,15 @@ public class TaskController {
         this.taskService = taskService;
     }
 
+    @Operation(summary = "Get tasks by project ID", description = "Retrieve a list of tasks associated with a specific project")
     @GetMapping("/project/{projectId}")
     public List<TaskDTO> getTaskByProject(@PathVariable Long projectId) {
         List<Task> taks = taskService.getTasksByProjectId(projectId);
         return taks.stream().map(task -> TaskMapper.toTaskDTO(task)).collect(Collectors.toList());
     }
 
+
+    @Operation(summary = "Create a new task under a project", description = "Create a new task and associate it with a specific project")
     @PostMapping("/project/{projectId}")
     public TaskDTO createTask(@PathVariable Long projectId, @RequestBody Task task) {
        
@@ -45,6 +53,7 @@ public class TaskController {
 
     }
 
+    @Operation(summary = "Update task status", description = "Update the status of a specific task")
     @PutMapping("/{taskId}/status")
     public TaskDTO updateTaskStatus(@PathVariable Long taskId, @RequestParam String newStatus) {
         
